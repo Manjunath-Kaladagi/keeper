d9d76da795e7b11413a699ae909b1b3b54730c52
Manjunath-Kaladagi <manju.g.kaladagi@gmail.com>
2022-12-29 22:30:18 +0530

    final styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import Zoom from "@mui/material/Zoom";
+
 import Note from "./Note";
 
 function CreateArea() {
   const [notes, setNotes] = useState([]);
   const [note, setNote] = useState({ title: "", content: "" });
+  const [isClicked, setisClicked] = useState(false);
 
   function handleChange(e) {
     let { name, value } = e.target;
     setNote((prev) => {
       return { ...prev, [name]: value };
     });
+  }
+
+  function handleClick() {
+    setisClicked(true);
   }
 
   function add(e) {
@@ -29,21 +38,28 @@ function CreateArea() {
 
   return (
     <div>
-      <form>
-        <input
-          value={note.title}
-          onChange={handleChange}
-          name="title"
-          placeholder="Title"
-        />
+      <form className="create-note">
+        {isClicked && (
+          <input
+            value={note.title}
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+          />
+        )}
         <textarea
+          onClick={handleClick}
           onChange={handleChange}
           value={note.content}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isClicked ? "3" : "1"}
         />
-        <button onClick={add}>Add</button>
+        <Zoom in={isClicked ? true : false}>
+          <Fab onClick={add}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
       {notes.map((note, index) => (
         <Note
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,13 @@
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+
 function Note(props) {
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={() => props.deleteNote(props.id)}>DELETE</button>
+      <button onClick={() => props.deleteNote(props.id)}>
+        <DeleteOutlineIcon />
+      </button>
     </div>
   );
 }
