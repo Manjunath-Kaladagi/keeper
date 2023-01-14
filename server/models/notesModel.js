484e3a4060dc24d5a2ac4b8ea0b3278a9f37c022
Manjunath-Kaladagi <manju.g.kaladagi@gmail.com>
2023-01-14 17:30:46 +0530

    added timestamp

--- server/models/notesModel.js
+++ server/models/notesModel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const notesSchema = new mongoose.Schema({
-  title: {
-    type: String,
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
   },
-  content: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Note", notesSchema);
