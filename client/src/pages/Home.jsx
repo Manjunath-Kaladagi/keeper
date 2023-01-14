832d539d515ea62cd18881e82e9a8fae86e918f0
Manjunath-Kaladagi <manju.g.kaladagi@gmail.com>
2023-01-14 17:25:35 +0530

    load create delete: working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,39 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import CreateArea from "../components/CreateArea";
+import Note from "../components/Note";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const fetchNotes = async () => {
+    const response = await fetch("http://localhost:8000/api/notes/");
+    const data = await response.json();
+    if (response.ok) {
+      setNotes(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea load={fetchNotes} />
+      {notes.map((note) => {
+        return (
+          <Note
+            key={note._id}
+            id={note._id}
+            title={note.title}
+            content={note.content}
+            load={fetchNotes}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
