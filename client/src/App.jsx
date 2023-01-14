220b4806b99c8b66a914465e648d9353adec5dbb
Manjunath-Kaladagi <manju.g.kaladagi@gmail.com>
2023-01-14 14:48:12 +0530

    fornt end setup/restructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import CreateArea from "./components/CreateArea";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div>
-      <Header />
-      <CreateArea />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import CreateArea from "../components/CreateArea";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
